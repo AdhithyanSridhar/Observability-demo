22d9ca65074cccf3ee1a9429b7433873ce3395a9
Adhithyan Sridhar <23568921+AdhithyanSridhar@users.noreply.github.com>
2021-03-04 10:06:27 +0530

    first commit

---
+++ src/main/java/com/example/observable/ObservablityApplication.java
@@ -0,0 +1,13 @@
+package com.example.observable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservablityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObservablityApplication.class, args);
+	}
+
+}
